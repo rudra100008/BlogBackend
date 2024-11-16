d3d5b2dbd216083e2e90b294d90b835897a1fa03
Ashum Bhuju Shrestha <136584855+rudra100008@users.noreply.github.com>
2024-11-16 09:58:24 +0545

    Update AppConfig.java

--- src/main/java/com/blogrestapi/Config/AppConfig.java
+++ src/main/java/com/blogrestapi/Config/AppConfig.java
@@ -3,28 +3,20 @@ package com.blogrestapi.Config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return  new BCryptPasswordEncoder();
-    }
-
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){return  new BCryptPasswordEncoder();}
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
